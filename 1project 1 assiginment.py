ee912f0bc3d89a8bb7259361c4105004c6b58937
Sri Ram Chandru.A <asriramchandru85@gmail.com>
2025-12-30 22:24:26 +0530

    Delete 1project 1 assiginment.py

--- 1project 1 assiginment.py
+++
@@ -1,7 +0,0 @@
-
-number1= int(input("enter your number 1:" ))
-number2= int(input("enter your number 2:"))
-print("addition=",number1 + number2)
-print("subtraction=",number1 - number2)
-print("multiplication=",number1 * number2)
-print("division=",number1/ number2)
